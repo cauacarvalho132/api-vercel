577c3506a4a4bc1dfe2c3dc9f8e050e1231b509c
cauacarvalho132 <cauagimenesdecarvalho@gmail.com>
2024-08-27 10:35:53 -0300

    a

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
- import express from 'express';
-import {Listar, ConsultarId,} from './service/veiculos.js';
-import {lista, ConsultaComId, NomeMontadora} from './service/montadoras.js';
+import express from 'express';
+import {montadoras, veiculos}  from './dados.js';
 
 const app = express();
 
-app.get('/veiculos', (req, res) => {
+app.get('/',(req,res)=>{
+    res.json({'Status': "Server OK"});
+});
 
-    res.status(200).json(Listar())
+app.get('/montadoras',(req,res)=>{
+    res.json(montadoras);
+});
+
+app.get('/veiculos',(req,res)=>{
+    res.json(veiculos);
 });
 
 
-app.get('/veiculos/:id', (req, res) => {
-    let id = req.params.id;
-    let resultconsulta = ConsultarId(id);
-    if (resultconsulta) {
-        res.status(200).json(resultconsulta);
-    } else {
-        res.status(404).json({
-            "erro": "veiculo não encontrado"
-        });
-    }
-})
-
-
-
-app.get('/montadoras', (req, res) => {
-
-    res.status(200).json(lista())
-});
-
-
-app.get('/montadoras/:id', (req, res) => {
-    let id = req.params.id;
-    let resultconsulta = ConsultaComId(id);
-    if (resultconsulta) {
-        res.status(200).json(resultconsulta);
-    } else {
-        res.status(404).json({
-            "erro": "fabricante não encontrado"
-        });
-    }
-})
-
-app.listen(80);
+app.listen(300, 'localhost');
